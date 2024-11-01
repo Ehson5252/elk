5d6a9b0735f566d41dffc5a3dd2f38dd92c14908
Ehson5252 <oehsoni1@gmail.com>
2024-11-01 12:20:53 -0500

    added shoping cart, star, search bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './header.css'
 import logo from '../../assets/logo_white.png'
+import cart from '../../assets/bag.png'
+import star from '../../assets/star.png'
 
 const Header = () => {
   return (
     <div className='header'>
       <img className='logo' src={logo} />
+      <input type="text" className='search' />
+      <img src={star} className="star" alt="star" />
+      <div className='cart'>
+        <span>0</span>
+        <img  src={cart} />
+      </div>
+
     </div>
   )
 }
